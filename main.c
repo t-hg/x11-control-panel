d70b195435c29eca848549cf9a61d05f72043824
Tobias Hort-Giess <tobias.hortgiess@gmail.com>
2023-10-01 21:46:00 +0200

    use high level pamixer

--- main.c
+++ main.c
@@ -1,25 +1,33 @@
 #include <gtk/gtk.h>
-#include <alsa/asoundlib.h>
 
-struct Sound {
-  snd_mixer_t *mixer;
-  snd_mixer_elem_t *mixer_elem;
-  snd_mixer_selem_id_t *sid;
-  long min, max;
-} sound;
+GtkStatusIcon *status_icon;
 
-gint get_volume() {
-  long vol;
-  snd_mixer_selem_get_playback_volume(sound.mixer_elem, 0, &vol);
-  long value = vol * 100 / sound.max;
-  return value;
+int get_volume() {
+  FILE *f = popen("pamixer --get-volume", "r");
+  if (f == NULL) {
+    fprintf(stderr, "[get_volume] popen failed\n");
+    return 0;
+  }
+  char buf[128];
+  while(fgets(buf, sizeof(buf), f) != NULL) {
+  }
+  if (pclose(f) != 0) {
+    fprintf(stderr, "[get_volume] pamixer --get-volume failed\n");
+    return 0;
+  }
+  return atoi(buf);
 }
 
-void set_volume(gint value) {
-  snd_mixer_selem_set_playback_volume_all(sound.mixer_elem, value * sound.max / 100);
+void set_volume(int value) {
+  char cmd[128];
+  sprintf(cmd, "pamixer --set-volume %d", value);
+  if(system(cmd) != 0) {
+    fprintf(stderr, "[set-volume] pamixer --set-volume failed\n");
+  }
 }
 
-void set_status_icon_icon(GtkStatusIcon *status_icon, gint volume) {
+gboolean set_status_icon_icon(gpointer user_data) {
+  int volume = get_volume();
   if (volume == 0) {
     gtk_status_icon_set_from_icon_name(status_icon, "audio-volume-muted");
   } else if (volume <= 33) {
@@ -29,17 +37,17 @@ void set_status_icon_icon(GtkStatusIcon *status_icon, gint volume) {
   } else if (volume <= 100) {
     gtk_status_icon_set_from_icon_name(status_icon, "audio-volume-high");
   }
+  return TRUE;
 }
 
-gchar* format_value(GtkWidget *scale, gdouble value, gpointer user_data) {
+gchar* scale_format_value(GtkWidget *scale, gdouble value, gpointer user_data) {
   // hide value
   return g_strdup_printf("");
 }
 
-void on_volume_changed(GtkWidget *scale, gpointer user_data) {
+void scale_on_volume_changed(GtkWidget *scale, gpointer user_data) {
   gint value = (gint) gtk_range_get_value(GTK_RANGE(scale));
   set_volume(value);
-  set_status_icon_icon(GTK_STATUS_ICON(user_data), get_volume());
 }
 
 void destroy_menu(GtkWidget *window, GdkEvent *event, gpointer user_data) {
@@ -59,8 +67,8 @@ void make_menu(GtkStatusIcon *status_icon, guint button, guint activate_time, gp
   GtkWidget *scaleVolume = gtk_scale_new_with_range(GTK_ORIENTATION_HORIZONTAL, 0, 100, 1);
   gtk_scale_set_value_pos(GTK_SCALE(scaleVolume), GTK_POS_RIGHT);
   gtk_range_set_value(GTK_RANGE(scaleVolume), get_volume());
-  g_signal_connect(G_OBJECT(scaleVolume), "format-value", G_CALLBACK(format_value), NULL);
-  g_signal_connect(G_OBJECT(scaleVolume), "value-changed", G_CALLBACK(on_volume_changed), status_icon);
+  g_signal_connect(G_OBJECT(scaleVolume), "format-value", G_CALLBACK(scale_format_value), NULL);
+  g_signal_connect(G_OBJECT(scaleVolume), "value-changed", G_CALLBACK(scale_on_volume_changed), status_icon);
   GtkWidget *vbox = gtk_vbox_new(FALSE, 0);
   gtk_container_add(GTK_CONTAINER(vbox), labelVolume);
   gtk_container_add(GTK_CONTAINER(vbox), scaleVolume);
@@ -77,42 +85,16 @@ void make_menu(GtkStatusIcon *status_icon, guint button, guint activate_time, gp
 }
 
 void make_status_icon(void){
-  GtkStatusIcon *status_icon = gtk_status_icon_new();
+  status_icon = gtk_status_icon_new();
   g_signal_connect(G_OBJECT(status_icon), "activate", G_CALLBACK(make_menu), NULL);
   g_signal_connect(G_OBJECT(status_icon), "popup-menu", G_CALLBACK(make_menu), NULL);
-  set_status_icon_icon(status_icon, get_volume());
   gtk_status_icon_set_visible(status_icon, TRUE);
-}
-
-void init_sound(void) {
-  snd_mixer_t *mixer;
-  snd_mixer_open(&mixer, 0);
-  snd_mixer_attach(mixer, "default");
-  snd_mixer_selem_register(mixer, NULL, NULL);
-  snd_mixer_load(mixer);
-  snd_mixer_selem_id_t *sid;
-  snd_mixer_selem_id_alloca(&sid);
-  snd_mixer_selem_id_set_index(sid, 0);
-  snd_mixer_selem_id_set_name(sid, "Master");
-  snd_mixer_elem_t* mixer_elem = snd_mixer_find_selem(mixer, sid);
-  long min, max;
-  snd_mixer_selem_get_playback_volume_range(mixer_elem, &min, &max);
-  sound.mixer = mixer;
-  sound.sid = sid;
-  sound.mixer_elem = mixer_elem;
-  sound.min = min;
-  sound.max = max;
-}
-
-void deinit_sound(void) {
-  snd_mixer_close(sound.mixer);
 }
 
 int main(int argc, char **argv) {
   gtk_init(&argc, &argv);
-  init_sound();
   make_status_icon();
+  gdk_threads_add_timeout(1000, set_status_icon_icon, NULL);
   gtk_main();
-  deinit_sound();
   return 0;
 }
